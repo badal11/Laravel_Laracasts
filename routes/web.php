d8aebdccf953795f783394cd5406cf94dbcfe271
badal <badaltripathi1@gmail.com>
2020-05-11 21:41:06 +0545

    Form to Create an Article

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('about',function (){
     ]);
 });
 
-Route::get('/articles/{articles}','ArticlesController@show');
 Route::get('/articles','ArticlesController@index');
+Route::get('/articles/create','ArticlesController@create');
+Route::get('/articles/{articles}','ArticlesController@show');
 
