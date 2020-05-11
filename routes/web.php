a3fa8bd78583f95ffd4959147e64433ae6deb23c
badal <badaltripathi1@gmail.com>
2020-05-11 14:48:35 +0545

    Render Dynamic Data
    ->created route for about_us
    ->displayed dynamic articles in about page.

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,9 @@ Route::get('/contact',function (){
 });
 
 Route::get('about',function (){
-    return view('about');
+    $articles = App\Article::take(3)->latest()->get();
+    return view('about',[
+        'articles'=>$articles
+    ]);
 });
+
