ece54dad82ad193dc238de6ad89f505ac187b0dc
badal <badaltripathi1@gmail.com>
2020-05-11 12:37:39 +0545

    Set an Active Menue Link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,11 +31,12 @@ Released   : 20140225
         </div>
         <div id="menu">
             <ul>
-                <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-                <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-                <li><a href="/about" accesskey="3" title="">About Us</a></li>
-                <li><a href="#" accesskey="4" title="">Careers</a></li>
-                <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+{{--                <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>--}}
+                <li class="{{ Request::path() === '/' ? 'current_page_item' : ''}} "><a href="/" accesskey="1" title="">Homepage</a></li>
+                <li ><a href="#" accesskey="2" title="">Our Clients</a></li>
+                <li class="{{ Request::path() === 'about' ? 'current_page_item' : '' }} "><a href="/about" accesskey="3" title="">About Us</a></li>
+                <li class="{{ Request::is('articles*') ? 'current_page_item' : '' }} "><a href="/articles" accesskey="4" title="">Articles</a></li>
+                <li class="{{ Request::path() === 'contact' ? 'current_page_item' : '' }} "><a href="/contact" accesskey="5" title="">Contact Us</a></li>
             </ul>
         </div>
     </div>
