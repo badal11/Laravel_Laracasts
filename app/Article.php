e78bbfef91d5191fde7a0be05610233075537af8
badal <badaltripathi1@gmail.com>
2020-05-13 17:49:58 +0545

    Linking User and Article
    ->adding 'user_id' column in Articles table.
    ->adding 'user_id' in ArticleFactory.

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'excerpt', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
